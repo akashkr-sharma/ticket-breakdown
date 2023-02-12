934ab5881aa79b93306402714bb4a5a0906e16ea
akashkr-sharma <125183911+akashkr-sharma@users.noreply.github.com>
2023-02-12 19:07:22 +0530

    unit test added

--- dpk.js
+++ dpk.js
@@ -1,19 +1,34 @@
 const crypto = require("crypto");
 
-exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
+const deterministicPartitionKey = (event) => {
   let candidate;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+  if(event){
+     candidate = getCandidatefromEvent(event);
   }
 
+  
+  candidate = getCandidate(candidate);
+  
+  return candidate;
+};
+
+const getCandidatefromEvent = (event) => {
+
+  if (event.partitionKey) {
+    candidate = event.partitionKey;
+  } else {
+    const data = JSON.stringify(event);
+    candidate = crypto.createHash("sha3-512").update(data).digest("hex");
+  }
+
+  return candidate;
+}
+
+
+const getCandidate = (candidate) => {
+  const TRIVIAL_PARTITION_KEY = "0";
+  const MAX_PARTITION_KEY_LENGTH = 256;
   if (candidate) {
     if (typeof candidate !== "string") {
       candidate = JSON.stringify(candidate);
@@ -24,5 +39,13 @@ exports.deterministicPartitionKey = (event) => {
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
     candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
   }
+
   return candidate;
-};
+}
+
+
+
+module.exports = {
+  deterministicPartitionKey
+}
+
--- dpk.test.js
+++ dpk.test.js
@@ -1,8 +1,34 @@
 const { deterministicPartitionKey } = require("./dpk");
+const crypto = require("crypto");
 
 describe("deterministicPartitionKey", () => {
   it("Returns the literal '0' when given no input", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
+  });
+
+  it("Returns trivialKey partitionKey as string", () => {
+    const trivialKey = deterministicPartitionKey({partitionKey: "akash-kr-01-sharma"});
+    expect(trivialKey).toBe("akash-kr-01-sharma");
+  });
+
+  it("Returns trivialKey when partitionKey present as object ", () => {
+    const event = {partitionKey: {name: "akash"}}
+    const trivialKey = deterministicPartitionKey(event);
+    const MAX_PARTITION_KEY_LENGTH = 256;
+    let data = JSON.stringify(event.partitionKey);
+    expect(trivialKey).toBe(data);
+  });
+
+  it("Returns trivialKey when partitionKey not present as object ", () => {
+    const event = { data: { name: 'akash-sharma' }}
+    const trivialKey = deterministicPartitionKey(event);
+    const MAX_PARTITION_KEY_LENGTH = 256;
+
+
+    const data = JSON.stringify(event);
+    let candidate = crypto.createHash("sha3-512").update(data).digest("hex");
+
+    expect(trivialKey).toBe(candidate);
   });
 });
